447ad1011c1135adf655b4941f3230246f1efffc
Germán <germancastromontes@gmail.com>
2019-02-25 13:19:38 +0100

    first comiit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Frase({data}) {
+
+  const {quote, author} = data
+
+
+  return (
+    <div className="frase">
+      <h1>{quote}</h1>
+      <p>- {author}</p>
+    </div>
+  )
+}
+
+
+function App() {
+
+  const [frase, setFrase] = useState({})
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    setFrase(resultado.data[0]);
   }
+
+  useEffect( 
+    () => {
+      consultarAPI();
+    }, []
+  )
+
+  return (
+    <div className="contenedor">
+      <Frase 
+        data={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >Generar nueva frase</button>
+    </div>
+  )
 }
 
 export default App;
